65edf10fa7c42436143fb991b610f948e52844da
DaniilKhlestkov <khlestkov00@inbox.ru>
2023-12-07 16:14:01 +0300

    zxc

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -9,21 +9,40 @@ import java.util.List;
 public class CarServiceImpl implements CarService{
     @Override
     public List<Car> getCar(int number) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Peugeot", "FWD", 308));
-        cars.add(new Car("BMW", "RWD", 2));
-        cars.add(new Car("Toyota", "4WD", 4));
-        cars.add(new Car("volkswagen", "AWD", 7));
 
-        List<Car> subCars = new ArrayList<>();
+        Car car1 = new Car("Peugeot", "FWD", 308);
+        Car car2 = new Car("BMW", "RWD", 2);
+        Car car3 = new Car("Toyota", "4WD", 4);
+        Car car4 = new Car("volkswagen", "AWD", 7);
 
-        if (number < cars.size()) {
-            for (int i = 0; i < number; i++) {
-                subCars.add(cars.get(i));
-            }
-            return subCars;
-        } else {
-            return cars;
+        List<Car> myCars;
+
+        {
+            myCars = new ArrayList<>();
+            myCars.add(car1);
+            myCars.add(car2);
+            myCars.add(car3);
+            myCars.add(car4);
+        }
+        switch (number) {
+            case (1):
+                myCars.subList(1,5).clear();
+                return myCars;
+
+            case (2):
+                myCars.subList(2,5).clear();
+                return myCars;
+
+            case (3):
+                myCars.subList(3,5).clear();
+                return myCars;
+
+            case (4):
+                myCars.subList(4,5).clear();
+                return myCars;
+
+            default:
+                return myCars;
         }
     }
 }
